7f5f7f587cf1756a7e90d7e65bdf6534c79a081c
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-02-18 06:06:55 -0600

    Refactor skeleton to formula class

--- src/formulas/formula_base.py
+++ src/formulas/formula_base.py
@@ -1,20 +1,34 @@
 from abc import ABC, abstractmethod
 import logging
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "utils")))
+from process import capture_subprocess_output
 
 class Formula_Base:
-    def __init__(self, name: str, app_cmd: list):
+    def __init__(self, name: str, build_script: str, app_cmd: list):
         # Private
         self.__name = name # name of the run
+        self.__build_script = build_script # script to build the application
         self.__app_cmd:list = app_cmd # command to execute application
 
         # Public
         self.profiler:str = None
-        
+
     def get_app_cmd(self):
         return self.__app_cmd
+    def get_app_args(self):
+        parts = self.__app_cmd[1:]
+        return parts[1] if len(parts) > 1 else ""
     def get_app_name(self):
         return self.__name
+    def build(self):
+        success, result = capture_subprocess_output(self.__build_script)
+        return {
+            "success": success,
+            "result": result
+        }
     # ----------------------------------------------------
     # Required methods to be implemented by child classes
     # ----------------------------------------------------
@@ -35,8 +49,8 @@ class Formula_Base:
                 logging.error(f"Cannot resolve profiler {self.profiler}: GT_TUNING environment variable must be set to install dir.")
         else:
             logging.error(f"Profiler {self.profiler} not supported.")
-        
-        
+
+
     @abstractmethod
     def instrument_pass(self):
         """
@@ -48,5 +62,19 @@ class Formula_Base:
     def optimize_pass(self):
         """
         Optimize the application based on the data collected from the instrumentation pass.
+        """
+        pass
+
+    @abstractmethod
+    def compiler_pass(self):
+        """
+        Compiles the application.
+        """
+        pass
+
+    @abstractmethod
+    def validation_pass(self):
+        """
+        Validates the the application.
         """
         pass
---
+++ src/utils/process.py
@@ -0,0 +1,65 @@
+
+import subprocess
+import io
+import selectors
+
+def capture_subprocess_output(subprocess_args:list, new_env=None) -> tuple:
+    # Start subprocess
+    # bufsize = 1 means output is line buffered
+    # universal_newlines = True is required for line buffering
+    process = (
+        subprocess.Popen(
+            subprocess_args,
+            bufsize=1,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        )
+        if new_env == None
+        else subprocess.Popen(
+            subprocess_args,
+            bufsize=1,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+            env=new_env,
+        )
+    )
+
+    # Create callback function for process output
+    buf = io.StringIO()
+
+    def handle_output(stream, mask):
+        try:
+            # Because the process' output is line buffered, there's only ever one
+            # line to read when this function is called
+            line = stream.readline()
+            buf.write(line)
+            print(line.strip())
+        except UnicodeDecodeError:
+            # Skip this line
+            pass
+
+    # Register callback for an "available for read" event from subprocess' stdout stream
+    selector = selectors.DefaultSelector()
+    selector.register(process.stdout, selectors.EVENT_READ, handle_output)
+
+    # Loop until subprocess is terminated
+    while process.poll() is None:
+        # Wait for events and handle them with their registered callbacks
+        events = selector.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+
+    # Get process return code
+    return_code = process.wait()
+    selector.close()
+
+    success = return_code == 0
+
+    # Store buffered output
+    output = buf.getvalue()
+    buf.close()
+
+    return (success, output)
