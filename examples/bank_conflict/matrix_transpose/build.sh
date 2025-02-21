a6059286aee8c7b183d3e5d002b1d12055fc4cbc
coleramos425 <colramos@amd.com>
2025-02-21 12:48:08 -0600

    Add makefile to matrix transpose example and target instrumented binary
    Signed-off-by: coleramos425 <colramos@amd.com>

--- examples/bank_conflict/matrix_transpose/build.sh
+++ examples/bank_conflict/matrix_transpose/build.sh
@@ -2,4 +2,5 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
-hipcc -O3 -o matrix_transpose matrix_transpose.hip
+make clean
+make
--- tests/test_matrix_transpose.sh
+++ tests/test_matrix_transpose.sh
@@ -2,4 +2,5 @@ BASE_PATH=$(realpath ../examples/bank_conflict/matrix_transpose)
 
 ./maestro --name matrix_transpose \
       --script "$BASE_PATH/build.sh" \
-      -- "$BASE_PATH/matrix_transpose"
+      -vv \
+      -- "$BASE_PATH/matrix_transpose_inst"
