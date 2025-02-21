adde7085910e24b7aa34c3824d63d59068913f12
Muhammad Awad <112003944+mawad-amd@users.noreply.github.com>
2025-02-21 15:16:35 -0800

    Add llm.c requirements  (#13)
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/bank_conflict/matrix_transpose/build.sh
+++ examples/bank_conflict/matrix_transpose/build.sh
@@ -2,4 +2,13 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
-hipcc -O3 -o matrix_transpose matrix_transpose.hip
+
+
+INST_BASE=/opt/logduration
+INCLUDE="-I ${INST_BASE}/include/kerneldb -I ${INST_BASE}/include/dh_comms -I /opt/rocm/include/hsa"
+NO_WARN=-Wno-unused-result
+
+
+INST_LIB=${INST_BASE}/lib/libAMDGCNMemTraceHip.so
+
+hipcc ${NO_WARN} ${INCLUDE} -fgpu-rdc -fpass-plugin=${INST_LIB} -O3 -g -o matrix_transpose matrix_transpose.hip
