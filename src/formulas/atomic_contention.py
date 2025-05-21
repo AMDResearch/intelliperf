9bc2d8e7235a7dd42aae5094845c44b382c9d004
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-05-21 17:41:00 +0000

    Add contention formula
    Signed-off-by: Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>

--- src/formulas/atomic_contention.py
+++ src/formulas/atomic_contention.py
@@ -22,29 +22,227 @@
 # SOFTWARE.
 ################################################################################
 
-from formulas.formula_base import Formula_Base
+import logging
+import os
+import pandas as pd
+import tempfile
+import time
+import sys
+import numpy as np
+import re
+import json
+
+from core.llm import LLM
+
+from formulas.formula_base import Formula_Base, Result, filter_json_field
+from utils.process import capture_subprocess_output, exit_on_fail
+from utils.regex import generate_ecma_regex_from_list
+
 
 class atomic_contention(Formula_Base):
-    def __init__(self, name: str, build_command: list, instrument_command: list, app_cmd: list, top_n: int):
-        super().__init__(name, build_command, instrument_command, app_cmd, top_n)
+    def __init__(self, name: str, build_command: list, instrument_command: list, project_directory: str, app_cmd: list, top_n: int, only_consider_top_kernel=False):
+        
+        super().__init__(name, build_command, instrument_command, project_directory, app_cmd, top_n)
+        
+        self._reference_app = self._application.clone()
 
-    def profile_pass(self):
+        self.profiler = "guided-tuning"
+        # This temp option allows us to toggle if we want a full or partial instrumentation report
+        self.only_consider_top_kernel = only_consider_top_kernel
+        self._instrumentation_results = None
+        self.current_kernel = None
+        self.current_args = None
+
+    def profile_pass(self) -> Result:
+        """
+        Profile the application using guided-tuning and collect atomic contention data
+
+        Returns:
+            Result: DataFrame containing the performance report card
+        """
         return super().profile_pass()
 
-    def instrument_pass(self):
-        return super().instrument_pass()
+    def instrument_pass(self) -> Result:
+        """
+        Instrument the application, targeting the kernels with the highest atomic contention data
 
-    def optimize_pass(self):
-        return super().optimize_pass()
+        Returns:
+            Result: Instrumentation data containing the kernel name, arguments, lines, and file path as dict
+        """
+        super().instrument_pass()
+     
+        return Result(success=False, asset=self._instrumentation_results)
 
-    def compile_pass(self):
+    def optimize_pass(self, temperature: float = 0.0, max_tokens: int = 3000) -> Result:
+        """
+        Optimize the kernel to remove atomic contention via OpenAI API
+
+        Args:
+            temperature (float): Sampling temperature for OpenAI API
+            max_tokens (int): Maximum tokens for OpenAI API
+
+        Returns:
+            Result: Optimized kernel as a file path
+        """
+
+        super().optimize_pass()
+        model = "gpt-4o"
+        llm_key  = os.getenv("LLM_GATEWAY_KEY")
+        
+        if not llm_key:
+            exit_on_fail(success=False, message="Missing LLM API key.")
+                
+        
+        system_prompt = (
+            "You are a skilled GPU HIP programmer. Given a kernel,"
+            " you will optimize it to remove atomic contention as much as possible"
+            " and provide a correct performant implementation. Do not modify"
+            " the kernel signature. Do not touch any other code, licenses, copyrights, or comments in the file." 
+            " If you remove the copyright, your solution will be rejected."
+            " Do not include any markdown code blocks or text other than the code."
+        )
+                        
+        server = "https://llm-api.amd.com/azure"
+        deployment_id = "dvue-aoai-001-o4-mini"
+        llm = LLM(
+            model=model,
+            api_key=llm_key,
+            system_prompt=system_prompt,
+            deployment_id=deployment_id,
+            server=server,
+        )
+
+        if self._instrumentation_results is None:
+            # Get the file from the results
+            field = "atomics"
+            subfield = "atomic_lat"
+            average_atomic_lat = 100
+            filtered_report_card = filter_json_field(self._initial_profiler_results, field, subfield, lambda x: x > average_atomic_lat)
+            
+            if len(filtered_report_card) == 0:
+                return Result(success=False, error_report="No atomic contention found.")
+            
+            logging.debug(f"Filtered Report Card:\n{filtered_report_card}")
+            kernel = filtered_report_card[0]["kernel"]
+            files = filtered_report_card[0]["source"]["files"]
+            kernel_name = kernel.split("(")[0]
+            kernel_file = None
+            for file in files:
+                if os.path.exists(file):
+                    with open(file, "r") as f:
+                        unoptimized_file_content = f.read()
+                        if kernel_name in unoptimized_file_content:
+                            kernel_file = file
+                            break
+            if kernel_file is None:
+                return Result(success=False, error_report=f"Kernel file not found.")
+            
+            user_prompt = (
+                f"There is an atomic contention in the kernel {kernel} in the file {unoptimized_file_content}."
+                f" Please fix the atomic contention but do not change the semantics of the program."
+                " If you remove the copyright, your solution will be rejected."
+            )
+            
+        else:
+            pass
+
+
+        logging.debug(f"LLM prompt: {user_prompt}")
+        logging.debug(f"System prompt: {system_prompt}")
+
+
+        self.current_kernel = kernel.split("(")[0]
+        self.current_args = kernel.split("(")[1].split(")")[0].split(",")
+
+        try:
+            optimized_file_content = llm.ask(user_prompt).strip()
+            with open(file, "w") as f:
+                f.write(optimized_file_content)
+            logging.debug(f"Optimized file content: {optimized_file_content}")
+            return Result(
+                success=True,
+                asset={
+                    "optimized_code_path": file,
+                    "optimized_code_string": optimized_file_content,
+                },
+            )
+        except Exception as e:
+            logging.error(f"An unexpected error occurred - {str(e)}")
+            return Result(success=False, error_report=f"An unexpected error occurred - {str(e)}")
+
+    def compiler_pass(self) -> Result:
+        """
+        Compile the application
+
+        Returns:
+            Result: Compilation status and the output file path
+        """
         return super().compile_pass()
 
-    def validation_pass(self):
-        return super().validation_pass()
+    def correctness_validation_pass(
+        self
+    ) -> Result:
+        """
+        Validate the optimized kernel by comparing the output with the reference kernel
 
-    def source_code_pass(self):
-        return super().source_code_pass()
-    
+        Args:
+            optimized_binary (str): File path of the optimized kernel
+            kernel (str): Kernel name
+            args (list): List of kernel arguments
+
+        Returns:
+            Result: Validation status
+        """
+        return super().correctness_validation_pass(self.current_kernel, self.current_args)
+
+    def performance_validation_pass(
+        self
+    ) -> Result:
+
+
+        unoptimized_time = self._initial_profiler_results[0]["durations"]["ns"]
+        
+        field = "atomics"
+        subfield = "atomic_lat"
+        unoptimized_metric = self._initial_profiler_results[0][field][subfield]
+
+        # Profile the optimized application
+        self._optimization_results = self._application.profile(top_n=self.top_n)
+        
+        optimized_time = self._optimization_results[0]["durations"]["ns"]
+        optimized_metric = self._optimization_results[0][field][subfield]
+
+        success = optimized_metric > unoptimized_metric
+        speedup = unoptimized_time / optimized_time
+        metric_improvement = (
+            optimized_metric / unoptimized_metric
+            if optimized_metric != 0
+            else 1
+        )
+
+        report_message = (
+            f"The optimized code contains {metric_improvement * 100}% fewer atomic contention."
+            f" The initial implementation contained {unoptimized_metric} atomic contention and"
+            f" the optimized code contained {optimized_metric} atomic contention."
+            f" The new code is {speedup:.3f}x faster than the original code. The initial"
+            f" implementation took {unoptimized_time} ns and the new one took"
+            f" {optimized_time} ns."
+        )
+
+        if not success:
+            return Result(
+                success=False,
+                error_report=f"The optimized code had more atomic contention."
+                + report_message,
+            )
+            
+        logging.info(report_message)
+        
+        return Result(success=True, asset={"log": report_message})
+
     def summarize_previous_passes(self):
-        return super().summarize_previous_passes()
+        """
+        Summarizes the results of the previous passes for future prompts.
+        """
+        pass
+
