518fcfd8a4a5c428119565c7e6a7d48698849cf7
Muhammad Awad <muhaawad@amd.com>
2025-07-01 22:03:22 +0000

    Upload artifact
    Signed-off-by: Muhammad Awad <muhaawad@amd.com>

--- .github/workflows/scripts/ci_tests.sh
+++ .github/workflows/scripts/ci_tests.sh
@@ -4,27 +4,29 @@ set -e
 # Run examples and store outputs
 echo 'Running IntelliPerf examples...'
 
+mkdir -p /intelliperf_results
 # Formulas
-intelliperf -vvv --project_directory=./examples --build_command="./scripts/build_examples.sh -c" --formula=memoryAccess -o /tmp/memory_access_output.json -- ./build/access_pattern/uncoalesced
-intelliperf -vvv --project_directory=./examples --build_command="./scripts/build_examples.sh -c" --formula=bankConflict -o /tmp/bank_conflict_output.json -- ./build/bank_conflict/matrix_transpose 1024 1024
+intelliperf -vvv --project_directory=./examples --build_command="./scripts/build_examples.sh -c" --formula=memoryAccess -o /intelliperf_results/memory_access_output.json -- ./build/access_pattern/uncoalesced
+intelliperf -vvv --project_directory=./examples --build_command="./scripts/build_examples.sh -c" --formula=bankConflict -o /intelliperf_results/bank_conflict_output.json -- ./build/bank_conflict/matrix_transpose 1024 1024
+intelliperf -vvv --project_directory=./examples --build_command="./scripts/build_examples.sh -c" --instrument_command="./scripts/build_examples.sh -i -c" --formula=atomicContention -o /intelliperf_results/atomic_contention_output.json -- ./build/contention/reduction
 
 
 # Diagnose Only
-intelliperf -vvv --formula=diagnoseOnly -o /tmp/diagnose_only_hip_uncoalesced.json -- ./build/access_pattern/uncoalesced
-intelliperf -vvv --formula=diagnoseOnly -o /tmp/diagnose_only_torch_add.json -- python ./examples/torch/add.py
-intelliperf -vvv --formula=diagnoseOnly -o /tmp/diagnose_only_triton_reduce.json -- python ./examples/triton/reduce.py
-
-
+intelliperf -vvv --formula=diagnoseOnly -o /intelliperf_results/diagnose_only_hip_uncoalesced.json -- ./build/access_pattern/uncoalesced
+intelliperf -vvv --formula=diagnoseOnly -o /intelliperf_results/diagnose_only_torch_add.json -- python ./examples/torch/add.py
+intelliperf -vvv --formula=diagnoseOnly -o /intelliperf_results/diagnose_only_triton_reduce.json -- python ./examples/triton/reduce.py
 
 # Display output files
 echo 'Memory Access Output:'
-cat /tmp/memory_access_output.json
+cat /intelliperf_results/memory_access_output.json
 echo 'Bank Conflict Output:'
-cat /tmp/bank_conflict_output.json
+cat /intelliperf_results/bank_conflict_output.json
+echo 'Atomic Contention Output:'
+cat /intelliperf_results/atomic_contention_output.json
 echo 'Diagnose Only Output:'
-cat /tmp/diagnose_only_hip_uncoalesced.json
-cat /tmp/diagnose_only_torch_add.json
-cat /tmp/diagnose_only_triton_reduce.json
+cat /intelliperf_results/diagnose_only_hip_uncoalesced.json
+cat /intelliperf_results/diagnose_only_torch_add.json
+cat /intelliperf_results/diagnose_only_triton_reduce.json
 
 # Success checking disabled for now
 echo 'Success checking disabled - just displaying outputs' 
