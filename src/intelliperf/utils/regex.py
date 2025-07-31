5297ecbbbdb5129bebef0c7ba3aaba97a8f13851
coleramos425 <colramos@amd.com>
2025-07-31 21:28:23 +0000

    Comply with linter
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/intelliperf/utils/regex.py
+++ src/intelliperf/utils/regex.py
@@ -38,8 +38,7 @@ def generate_ecma_regex_from_list(kernel_names: set) -> str:
 		# Note: Temporary fix, but until bug in omniprobe is fixed we need to also
 		# add the name of the instrumented kernel clone to the regex, otherwise we'll skip it
 		# and exclude it from the memory analysis report
-		# duplicate_kernel_str = f"__amd_crk_{i.replace(')', ', void*)', 1)}"
-		duplicate_kernel_str = f"__amd_crk_{i.replace(")", ", void*)", 1)}"
+		duplicate_kernel_str = f"__amd_crk_{i.replace(')', ', void*)', 1)}"
 		escaped_string = re.escape(duplicate_kernel_str)
 		regex_string = r"^" + escaped_string + r"$"
 		res.append(regex_string)
